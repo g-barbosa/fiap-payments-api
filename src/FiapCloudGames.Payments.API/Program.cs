22502f920defad7914cd68485e824374258aacc0
Giovanne Barbosa <giovanne.bbarbosa@gmail.com>
2026-06-08 22:07:27 -0300

    permite swagger em prod

--- src/FiapCloudGames.Payments.API/Program.cs
+++ src/FiapCloudGames.Payments.API/Program.cs
@@ -25,12 +25,8 @@ namespace FiapCloudGames.Payments.API
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
