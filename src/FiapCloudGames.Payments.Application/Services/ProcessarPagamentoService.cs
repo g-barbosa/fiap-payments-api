7874d416727759256580ad8abf4d0a9dc6a67e15
Giovanne Barbosa <giovanne.bbarbosa@gmail.com>
2026-06-22 20:12:04 -0300

    implementa processamento de pagamento

--- src/FiapCloudGames.Payments.Application/Services/ProcessarPagamentoService.cs
+++ src/FiapCloudGames.Payments.Application/Services/ProcessarPagamentoService.cs
@@ -1,17 +1,28 @@
-﻿using FiapCloudGames.Payments.Domain.Pedidos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FiapCloudGames.Payments.Domain.Events;
+using FiapCloudGames.Payments.Domain.Interfaces.Messaging;
+using FiapCloudGames.Payments.Domain.Pedidos;
 
 namespace FiapCloudGames.Payments.Application.Services
 {
     public class ProcessarPagamentoService
     {
+        private readonly IPagamentoProcessadoPublisher _pagamentoProcessadoPublisher;
+        public ProcessarPagamentoService(IPagamentoProcessadoPublisher pagamentoProcessadoPublisher)
+        {
+            _pagamentoProcessadoPublisher = pagamentoProcessadoPublisher;
+        }
         public async Task Processar(PedidoCriadoEvent evento)
         {
+            var status = Random.Shared.Next(2) == 0 ? "Aprovado" : "Recusado";
 
+            await _pagamentoProcessadoPublisher.PublicarPagamentoProcessadoAsync(new PagamentoProcessadoEvent
+            {
+                PedidoId = evento.PedidoId,
+                NomeUsuario = evento.NomeUsuario,
+                Email = evento.Email,
+                Valor = evento.Valor,
+                Status = status
+            });
         }
     }
 }
